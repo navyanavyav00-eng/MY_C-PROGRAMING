c0821c2ec6f027f9e3e74b4946d179f6ef30434a
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-18 11:28:19 +0530

    Implement if-else ladder for number classification

---
+++ if else ladder.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+  int a;
+  printf("enter the num\n");
+  scanf("%d",&a);
+  if(a>0)
+  { printf(" it is  positive number\n");
+      
+  }
+ else if(a<0)
+  { printf(" it is negative number\n");
+      
+  }
+ else
+ { printf(" it is a zero");}
+    return 0;
+}
