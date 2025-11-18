b49fae25d85188f62b22e41b8dcc19f5b7a8226b
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-18 12:36:47 +0530

    Add Fibonacci sequence program in C
    Implement Fibonacci sequence calculation in C.

---
+++ fibonacci.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() { int n,f3,f1=0,f2=1;
+printf("enter the number \n");
+scanf("%d",&n);
+
+while(n!=0)
+{ f3=f1+f2;
+printf("%d\t",f1);
+n--;
+ f1=f2;
+f2=f3;
+    
+}
+    return 0;
+}
