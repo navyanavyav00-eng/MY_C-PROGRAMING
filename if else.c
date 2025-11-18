f98ffc658260b37ab40d4ff5465056ce54cb2437
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-18 11:10:18 +0530

    Add C program to check if a number is even or odd

---
+++ if else.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int a;
+   printf("enter the num\n");
+   scanf("%d",&a);
+   if(a%2==0)
+{
+     printf("it is even the num\n");
+}
+else{printf("it is odd the num\n");}
+    return 0;
+}
