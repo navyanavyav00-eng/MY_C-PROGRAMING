4d0d39a8f0995b308e383f6f099d324b6e2e900d
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-18 20:13:22 +0530

    Create Circle.c

---
+++ Circle.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (){
+float  radius ,area;
+printf("enter the area of circle:");
+scanf("%f",&area);
+area=3.14*radius*radius;
+printf("enter the area of circle%.2f\n",area);
+}
