13872488b29b1256a7729212a85dd554a552b1d0
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-18 12:23:34 +0530

    Add continue statement example in C

---
+++ continue.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+ int main() {
+    int i;
+    for(i=1;i<=5;i++)
+    { if (i==2)
+     continue;
+  
+     printf("%d\n",i);
+        
+    }
+    
+  
+
+}
