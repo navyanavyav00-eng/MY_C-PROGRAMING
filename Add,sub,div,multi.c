dbd47824f9109886b2524cfddd57a2177c68d6f6
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-18 17:18:22 +0530

    Add basic arithmetic operations in C

---
+++ Add,sub,div,multi.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a=2,b=4;
+    printf("addition is:%d\n",a+b);
+    printf("sub is:%d\n",a-b);
+    printf("multi is:%d\n",a*b);
+    printf("div is:%d\n",a/b);
+
+
+
+}
