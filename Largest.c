9f7016d7ad2ad7529156e758c7d1d5141c3f0415
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-18 17:21:44 +0530

    Implement largest number finder in Largest.c
    Fix logic errors and improve input handling.

---
+++ Largest.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main ()
+{ int a,b,c,large;
+printf("enter an integer number");
+scanf("%d%d%d",&a,&b,&c,&large);
+if(a>b) (a>c)
+
+printf(" a is large\n",a);
+else if(b>c)
+printf("b is small\n",b);
+else
+printf("c is large\n",c);
+
+
+
+
+
+
+
+
+}
