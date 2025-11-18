370e558390bcc428a09d2d181e3d30a393f8b213
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-18 11:19:10 +0530

    Create nested if.c

---
+++ nested if.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int a=20;
+  if(a<30){
+    if(a>10){
+      printf(" %d\n",a);
+    }
+  }
+
+
+
+
+
+  
+}
