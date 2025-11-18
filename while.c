5777e0d8c9b29ee6f8e27414edec3e3cbe4ecc1e
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-18 11:38:35 +0530

    Add while loop to print numbers from 1 to n

---
+++ while.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int n, i = 1;
+   printf("enter the number\n");
+   scanf("%d",&n);
+   while(i<=n){
+       
+   printf("%d\n",i);
+   i++;}
+    return 0;
+}
