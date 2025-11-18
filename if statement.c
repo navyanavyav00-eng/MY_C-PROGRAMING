637f34a1b33643e295c1a1841154fbaf495e4d1f
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-18 10:52:10 +0530

    Correct if statement syntax
    Fix the if statement to remove the semicolon.

---
+++ if statement.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() { int a,i;
+     a=2;
+    i=6;
+    if(i>a);
+    printf(" i is greater than a");
+
+    return 0;
+}
