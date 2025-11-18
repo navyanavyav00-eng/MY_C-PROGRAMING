df37e037e277a744a1582d584cea06f51920c1a1
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-18 12:13:54 +0530

    Add break statement example in C

---
+++ break.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int i;
+   for(i=1; i<=5;i++){
+   printf("%d\n",i);
+   if(i==3)
+   break;}
+
+    return 0;
+}
