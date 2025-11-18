af86674d84a37b6b858fd1eeb0c7849afe5e4d04
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-18 09:13:18 +0530

    Create program to input and display user details
    Fix syntax errors and improve input handling.

---
+++ name,adress,location.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main ;
+main {
+char name[50];
+int age;
+char location;
+printf("enter your name");
+scanf("%s",name);    
+    
+printf("enter your age");
+scanf("%d",age);    
+        
+ printf("enter your location");
+scanf("%s",location);    
+        prithf("\n enter your details  \n");
+       
+     prithf("\n name: %s \n",name);
+     prithf("\n name: %d\n",age);
+       prithf("\n name: %s\n",location);
+       return o;
+          
+       
+    
+}
