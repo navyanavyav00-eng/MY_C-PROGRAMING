a0d98bf9c63cce2567ccd5bd467c861295dc5bb9
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-18 15:55:51 +0530

    Add user details input function in depart.c

---
+++ NAME,AGE,depart.c
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int userdetails()
+{ char name, depar;
+int age;
+printf("enter your name:\n");
+scanf("%s",&name);
+printf("enter your age:\n");
+scanf("%d",&age);
+
+printf("enter your department:\n");
+scanf("%s",&depar);
+
+
+
+}
+
+int main() { userdetails();
+   
+}
