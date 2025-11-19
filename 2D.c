9ded23c5c297746410fafbd65519ee9e89667eee
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-19 11:39:10 +0530

    Add 2D array example in C

---
+++ 2D.c
@@ -0,0 +1,25 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() { 
+    int pen[2] [3]={
+    { 1,2,3},
+    {4,5,6}
+    };
+    for( int i=0;i<2;i++)
+    { for(int j=0;j<3;j++)
+    { printf("%d",pen[i] [j]);
+    } printf("\n");
+    
+    }
+    
+    
+
+ 
+ 
+ 
+ 
+ 
+ 
+    return 0;
+}
