8018bb8db6d81c493a67efe1e20cd8ec2ad622e6
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-19 08:03:37 +0530

    Create Name,age, location.c

---
+++ Name,age, location.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{ char name ,location;
+int age;
+printf("enter the name\n");
+scanf("%s",&name);
+printf("enter the age\n");
+scanf("%d",&age);
+printf("enter the location\n");
+scanf("%s",&location);
+
+}
