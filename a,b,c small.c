f73a17a68bf4c67dabdc7461297c70ebb523f004
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-19 07:44:27 +0530

    Create a,b,c small.c

---
+++ a,b,c small.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main ()
+{
+int a,b,c,small;
+printf("enter the number\n");
+scanf("%d",&small);
+if(a<=b && a<=c)
+{
+printf("a is small\n",a);
+}
+else if(b<=c)
+{printf("b is small\n",b);
+}
+ else
+ { printf("c is smal\n",c);
+ }
+
+
+}
