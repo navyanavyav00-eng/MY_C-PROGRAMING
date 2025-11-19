ac4b3074e402e4ddeaa7c709207e8c9e366332c1
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-19 11:52:09 +0530

    Add function to determine largest of three integers
    Implement a function to find the largest of three numbers.

---
+++ Function large.c
@@ -0,0 +1,28 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+
+    int largest( int a,int b,int c)
+    { if(a>=b && a>=c)
+   {printf("a is largest\n");
+   scanf("%d",&a);
+    } 
+    if( b>=c && b>=c){
+    printf("b is largest\n");
+   scanf("%d",&b);
+    }
+    else
+    { printf(" c is largest\n");
+    }
+    }
+    
+    
+
+ 
+ int main (){
+      largest( 8,5,2);
+  
+  
+ 
+    return 0;
+}
