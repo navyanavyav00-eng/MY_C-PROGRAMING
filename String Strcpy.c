65f0941372d5d81288402dae387e2ab9f010423d
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-20 10:54:12 +0530

    Add Strcpy.c to demonstrate string copying

---
+++ String Strcpy.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){ 
+char  rose[20];
+printf("enter the string name\n");
+strcpy( rose," Navya");
+strcpy( rose," Deekshith");
+printf("%s",rose);
+
+
+
+
+
+}
