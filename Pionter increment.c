bd0e06eec6f841c60373ba8e8c2f239084b23be9
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-20 16:19:09 +0530

    Create Pionter increment.c

---
+++ Pionter increment.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int x[]={10,20,30};
+     int *p= x;
+     printf("%d ",*p);
+    p++;
+printf("%d ",*p);
+p++;
+printf("%d ",*p);
+    
+    return 0;
+}
