237bc99ae28ad88ff1a99befe7e7661fb72e13c6
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-20 11:57:47 +0530

    Create Pointer.c

---
+++ Pointer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{ int num =10;
+int *p;
+p =&num;
+printf(" value of num= %d\n",num);
+printf("adress of num =%p\n",&num);
+printf("pointet p stores=%p\n",p);
+printf("value using pointet=%d\n",*p);
+
+
+
+
+
+
+}
