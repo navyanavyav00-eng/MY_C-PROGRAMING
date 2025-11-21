dc947e3ad875bb4dffd51af200e02dfa598f3b00
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-21 15:06:02 +0530

    Create Robot  distance formula.c

---
+++ Robot  distance formula.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>   // For sqrt() function
+
+int main() {
+    float x1, y1, x2, y2, distance;
+
+    printf("Enter x1 and y1: ");
+    scanf("%f %f", &x1, &y1);
+
+    printf("Enter x2 and y2: ");
+    scanf("%f %f", &x2, &y2);
+
+    distance = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+
+    printf("The distance between the two points = %.2f\n", distance);
+
+    return 0;
+}
