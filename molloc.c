827418c50f874a41b0bd6cb0d2852b90ca28dabd
navyanavyav00-eng <navyanavyav00@gmail.com>
2025-11-21 10:15:20 +0530

    Create molloc.c

---
+++ molloc.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<stdlib.h>
+
+int main() {
+int * p=(int*)malloc(5*sizeof(int));
+*p=10;
+printf("%d\n", *p);
+
+return 0;
+
+} 
